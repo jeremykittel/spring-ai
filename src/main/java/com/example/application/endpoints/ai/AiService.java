6b00541d48d43b0f218bcfdc2f704c5582eabd31
Jeremy Kittel <jeremy.kittel@solutiondesign.com>
2024-04-02 10:05:41 -0500

    refactored to move prompt into template resource

--- src/main/java/com/example/application/endpoints/ai/AiService.java
+++ src/main/java/com/example/application/endpoints/ai/AiService.java
@@ -4,13 +4,14 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.ChatClient;
-import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,32 +24,31 @@ public class AiService {
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
 
+    @Value("classpath:prompts/rag-prompt-template.st")
+    private Resource ragPromptTemplate;
+
     public AiService(ChatClient chatClient, VectorStore vectorStore) {
         this.chatClient = chatClient;
         this.vectorStore = vectorStore;
     }
 
+
+    /**
+     * Service method used for communicating with ChatGPT and embedding context
+     *
+     * @param query the prompt to send to openai api
+     * @return the response with augmented data
+     */
     public String chat(String query) {
-
         List<Document> similarDocuments = vectorStore.similaritySearch(query);
-
-        String information = similarDocuments.stream()
+        String contentList = similarDocuments.stream()
                 .map(Document::getContent)
                 .collect(Collectors.joining(System.lineSeparator()));
-
-        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(
-                """
-                            You are a helpful assistant.
-                            Use only the following information to answer the question.
-                            Do not use any other information. If you do not know, simply answer: Unknown.
-
-                            {information}
-                        """);
-
-        Message systemMessage = systemPromptTemplate.createMessage(Map.of("information", information));
-        PromptTemplate promptTemplate = new PromptTemplate("{query}");
-        Message message = promptTemplate.createMessage(Map.of("query", query));
-        Prompt prompt = new Prompt(List.of(systemMessage, message));
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+        Map<String, Object> promptParameters = new HashMap<>();
+        promptParameters.put("input", query);
+        promptParameters.put("documents", String.join("\n", contentList));
+        Prompt prompt = promptTemplate.create(promptParameters);
         return chatClient.call(prompt).getResult().getOutput().getContent();
     }
 }
