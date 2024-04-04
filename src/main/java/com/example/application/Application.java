cc29abf59d30f09f9782724e12f9b971acc73d4a
Jeremy Kittel <jeremy.kittel@solutiondesign.com>
2024-04-04 17:33:38 -0500

    changed theme name and added pwa annotation

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,6 +1,7 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  */
+@PWA(name = "Sandbox", shortName = "Sandbox")
 @SpringBootApplication
-@Theme(value = "hilla-react-app")
+@Theme(value = "sandbox-theme")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
