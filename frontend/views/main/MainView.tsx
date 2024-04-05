3eb1d8a615e750bc4fffb70576839aa713c5b69f
Jeremy Kittel <jeremy.kittel@solutiondesign.com>
2024-04-04 20:26:41 -0500

    refactored css maintains the same functionality and behavior, but it makes the code cleaner, easier to read, and maintain.

--- frontend/views/main/MainView.tsx
+++ frontend/views/main/MainView.tsx
@@ -1,7 +1,7 @@
 export default function MainView() {
     return (
         <div className="flex flex-col h-full items-center justify-center text-center box-border">
-            <img src="images/Jeremy-Signature.png"/>
+            <img src="images/Jeremy-Signature.png" alt={"Jeremy Kittel Signature"}/>
             <q>The biggest room in the world is the room for improvement.</q>
         </div>
     );
