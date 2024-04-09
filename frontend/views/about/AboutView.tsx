13fbe92313e3ae6d07648b4d3c472d835c52d12e
Jeremy Kittel <jeremy.kittel@solutiondesign.com>
2024-04-09 14:37:21 -0500

    added alt to image tag

--- frontend/views/about/AboutView.tsx
+++ frontend/views/about/AboutView.tsx
@@ -2,7 +2,7 @@ import css from './AboutView.module.css';
 export default function AboutView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img className={css.img} src="images/Jeremy.png"/>
+      <img className={css.img} src="images/Jeremy.png" alt="Jeremy sitting in chair"/>
       <h2>This place intentionally left empty</h2>
       <p>It’s a place where you can grow your own UI 🤗</p>
     </div>
