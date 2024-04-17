f455e5be4ef5eee4d2bb94caa7c655c9450e4718
Jeremy Kittel <jeremy.kittel@solutiondesign.com>
2024-04-16 21:44:55 -0500

    Add @DirtiesContext annotation and RabbitMQ configuration
    Added @DirtiesContext annotation in BaseIntegrationTestConfig to ensure context is rebuilt for each test. Also redundant tests in ApplicationIntegrationTest were removed. RabbitMQ configurations were added to the application.properties file for specifying the host, port, username, and password.

--- src/test/java/com/example/application/integration/ApplicationIntegrationTest.java
+++ src/test/java/com/example/application/integration/ApplicationIntegrationTest.java
@@ -1,15 +1,8 @@
 package com.example.application.integration;
 
-import com.example.application.Application;
 import org.junit.jupiter.api.Test;
 
 public class ApplicationIntegrationTest extends BaseIntegrationTestConfig {
     @Test
-    void contextLoads() {
-    }
-
-    @Test
-    public void applicationContextTest() {
-        Application.main(new String[] {});
-    }
+    void contextLoads() {}
 }
--- src/test/java/com/example/application/integration/BaseIntegrationTestConfig.java
+++ src/test/java/com/example/application/integration/BaseIntegrationTestConfig.java
@@ -3,6 +3,7 @@ package com.example.application.integration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.RabbitMQContainer;
@@ -10,6 +11,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@DirtiesContext
 public class BaseIntegrationTestConfig {
 
     public static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3-management-alpine");
