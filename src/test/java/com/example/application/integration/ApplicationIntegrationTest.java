c31c3463872725b37e83f6964c61acfbe7552247
Jeremy Kittel <jeremy.kittel@solutiondesign.com>
2024-04-16 21:06:48 -0500

    Remove unused imports in ApplicationIntegrationTest
    Unused imports from ApplicationIntegrationTest.java file got removed to keep the code clean and efficient. This includes the removal of "@RunWith(SpringRunner.class)" and other unnecessary import statements, contributing to better readability and maintainability of the code.

--- src/test/java/com/example/application/integration/ApplicationIntegrationTest.java
+++ src/test/java/com/example/application/integration/ApplicationIntegrationTest.java
@@ -2,11 +2,7 @@ package com.example.application.integration;
 
 import com.example.application.Application;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
-
-@RunWith(SpringRunner.class)
 public class ApplicationIntegrationTest extends BaseIntegrationTestConfig {
     @Test
     void contextLoads() {
