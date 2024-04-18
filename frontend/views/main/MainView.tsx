4df51693274ec846782be1ce6a7aeb680f60991d
Jeremy Kittel <jeremy.kittel@solutiondesign.com>
2024-04-18 15:31:43 -0500

    Correct the quote citation in MainView
    Reformatted the inspirational quote in the MainView to include proper citation. The text is now wrapped with <p> tags, and a <cite> tag has been added attributing the quote to Helmut Schmidt, with a link to his Wikipedia page.

--- frontend/views/main/MainView.tsx
+++ frontend/views/main/MainView.tsx
@@ -2,7 +2,8 @@ export default function MainView() {
     return (
         <div className="flex flex-col h-full items-center justify-center text-center box-border">
             <img src="images/Jeremy-Signature.png" alt={"Jeremy Kittel Signature"}/>
-            <q>The biggest room in the world is the room for improvement.</q>
+            <p><q>The biggest room in the world is the room for improvement.</q> - <cite><a
+                href="https://en.wikipedia.org/wiki/Helmut_Schmidt" target="_blank">Helmut Schmidt</a></cite></p>
         </div>
     );
 }
